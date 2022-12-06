24094d61b20926bfc9f95f43fab6dc141029195b
Frick Microsoftveryhard <frodikarlsson@gmail.com>
2022-12-06 06:57:00 +0100

    Finish 6, clean up 5

--- src/main/scala/competitive/competitions/aoc/2022/5/Five.scala
+++ src/main/scala/competitive/competitions/aoc/2022/5/Five.scala
@@ -1,68 +1,41 @@
 package competitive.competitions.aoc.y2022
 import competitive.competitions.aoc.AocProblem
-import competitive.algorithms.matrix.*
-import competitive.algorithms.language.*
+import collection.mutable.Stack
 
 object Five extends AocProblem(2022, 5):
-    case class Box(letter: Char, empty: Boolean = false)
-    val instruction = Instruction[Unit]("move", 3)
-
     def parse(input: Vector[String]) = 
         val stacksIndex = input.indexWhere(!_.contains("["))
-        val stacks = input(stacksIndex).split("   ").map(_.replaceAll(" ", "").toInt).toVector
         val inputWithoutStacks = input.take(stacksIndex) ++ input.drop(stacksIndex + 1)
         val boxLines = inputWithoutStacks.take(inputWithoutStacks.indexWhere(_.isEmpty))
-        val boxes = for line <- boxLines.map(_.zipWithIndex.filter(_._2 % 4 == 1).map(_._1)) yield
-                line.map(l => Box(l, l == ' ')).toVector
-        val instructions = 
+        val boxes: Vector[Stack[Char]] = 
+            (for line <- boxLines.map(_.zipWithIndex.filter(_._2 % 4 == 1).map(_._1)).transpose yield
+                Stack.from(line.toVector.filterNot(_ == ' '))).toVector
+        val instructions: Vector[(Int, Int, Int)] = 
             inputWithoutStacks
-                .drop(stacksIndex + 1)
-                .map(
-                    _.split(" ").toVector
-                )
-                .map(i => (i(1).toInt, i(3).toInt, i(5).toInt))
-        (Matrix(boxes), instructions)
-
-    private def getStr(matrix: Matrix[Box]) = 
-        (for col <- 0 until matrix.cols yield
-            matrix.col(col).find(!_.empty).get.letter).mkString
-
-    private def execute(execution: (args: Vector[Int]) => Unit, instructions: Vector[(Int, Int, Int)]) =
-        for i <- instructions do
-            val call = InstructionCall[Unit, Int](instruction, Vector(i._1, i._2, i._3), execution).execute
+                .drop(stacksIndex + 1).map(_.split(" ").toVector).map(i => (i(1).toInt, i(3).toInt, i(5).toInt))
+        (boxes, instructions)
     
     override def part1(input: Vector[String]) =
         val parsed = parse(input)
-        val instructions = parsed._2
-        var matrix = parsed._1
+        val (stack, instructions) = parsed
         val execution = 
-            (args: Vector[Int]) =>
-                for i <- 0 until args(0) do
-                    val (from, to) = (args(1) - 1, args(2) - 1)
-                    if !matrix.col(to).exists(_.empty) then
-                        matrix = matrix.withRow(0, matrix.row(0).map(b => Box(' ', true)))
-                    matrix = matrix.swapped(
-                        (matrix.col(to).zipWithIndex.reverse.find(_._1.empty).get._2, to), 
-                        (matrix.col(from).indexWhere(!_.empty), from)
-                    )
-        execute(execution, instructions)
-        getStr(matrix)
+            (args: (Int, Int, Int)) =>
+                for i <- 0 until args._1 do
+                    stack(args._3 - 1).push(stack(args._2 - 1).pop())
+
+        instructions.foreach(execution(_))
+        stack.map(_.pop()).mkString("")
 
     override def part2(input: Vector[String]) =
         val parsed = parse(input)
-        val instructions = parsed._2
-        var matrix = parsed._1
+        val (stack, instructions) = parsed
         val execution = 
-            (args: Vector[Int]) =>
-                val boxesToTake = matrix.col(args(1) - 1).filter(!_.empty).take(args(0))
-                val emptySpaces: Int = matrix.col(args(2) - 1).count(_.empty)
-                for i <- 0 until boxesToTake.length - emptySpaces do
-                        matrix = matrix.withRow(0, matrix.row(0).map(b => Box(' ', true)))
-                for i <- 0 until boxesToTake.length do
-                    matrix = matrix.swapped(
-                        (matrix.col(args(2) - 1)
-                            .zipWithIndex.reverse.find(_._1.empty).get._2 - boxesToTake.length + i + 1, args(2) - 1),
-                        (matrix.col(args(1) - 1).indexWhere(!_.empty), args(1) - 1)
+            (args: (Int, Int, Int)) =>
+                val boxes = (for i <- 0 until args._1 yield stack(args._2 - 1).pop()).toVector.reverse   
+                for i <- 0 until args._1 do
+                    stack(args._3 - 1).push(
+                        boxes(i)
                     )
-        execute(execution, instructions)
-        getStr(matrix)
+
+        instructions.foreach(execution(_))
+        stack.map(_.pop()).mkString("")
--- src/main/scala/competitive/competitions/aoc/2022/6/Six.scala
+++ src/main/scala/competitive/competitions/aoc/2022/6/Six.scala
@@ -1,13 +1,15 @@
 package competitive.competitions.aoc.y2022
 import competitive.competitions.aoc.AocProblem
+import collection.mutable.Set
 
 object Six extends AocProblem(2022, 6):
     def parse(input: Vector[String]) = 
-        ???
+        input.head
 
     override def part1(input: Vector[String]) =
-        ???
+        (parse(input).sliding(4).indexWhere(g => g.distinct.length == 4) + 4).toString
 
     override def part2(input: Vector[String]) =
-        ???
+        (parse(input).sliding(14).indexWhere(g => g.distinct.length == 14) + 14).toString
+
 
