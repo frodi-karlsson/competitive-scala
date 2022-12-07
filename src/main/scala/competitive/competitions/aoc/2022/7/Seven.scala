74a02f51e54b882c2e9b0489f97888c37b2b0f6c
Frick Microsoftveryhard <frodikarlsson@gmail.com>
2022-12-07 12:08:25 +0100

    clean up seven

--- src/main/scala/competitive/competitions/aoc/2022/7/Seven.scala
+++ src/main/scala/competitive/competitions/aoc/2022/7/Seven.scala
@@ -14,39 +14,31 @@ object Seven extends AocProblem(2022, 7):
         val root = Directory("/")
         var dirs = Vector(root)
         var currentDir = root
-        input.tail.foreach { line =>
-            val prev = currentDir.parent
-            if line.startsWith("$ cd") then
-                val dirName = line match
-                    case s"$$ $cd $dir" => dir
-                    case _ => throw new Exception(s"Invalid cd command: $line")
-                if dirName == "/" then currentDir = root
-                else if dirName == ".." then currentDir = prev.getOrElse(throw new Exception(s"Invalid cd command: $line"))
-                else currentDir = currentDir.subDirs.find(_.name == dirName).getOrElse(throw new Exception(s"Invalid cd command: $line"))
-            else if line.startsWith("dir") then
-                val dirName = line match
-                    case s"dir $dir" => dir
-                    case _ => throw new Exception(s"Invalid dir command: $line")
-                val dir = Directory(dirName, parent = Some(currentDir))
-                currentDir.subDirs = currentDir.subDirs :+ dir
-                dirs = dirs :+ dir
-            else if line(0).isDigit then
-                val Array(size, fileName) = line.split(" ")
-                val file = File(fileName, size.toInt)
-                currentDir.addFile(file)
-            else if line != "$ ls" then throw new Exception(s"Invalid command: $line")
-        }
+        input.tail.foreach(line =>
+            line match
+                case s"$$ $cd $dir" => 
+                    if dir == ".." then currentDir = 
+                        currentDir.parent.getOrElse(throw new Exception(s"Invalid cd command: $line"))
+                    else currentDir =
+                            currentDir.subDirs.find(_.name == dir)
+                            .getOrElse(throw new Exception(s"Invalid cd command: $line"))
+                case s"dir $dir" => 
+                    val directory = Directory(dir, parent = Some(currentDir))
+                    currentDir.subDirs = currentDir.subDirs :+ directory
+                    dirs = dirs :+ directory
+                case "$ ls" =>
+                case s"${size} $file" => 
+                    val Array(size, fileName) = line.split(" ")
+                    val file = File(fileName, size.toInt)
+                    currentDir.addFile(file)
+                case _ => throw new Exception(s"Invalid cd command: $line")
+        )
         (root, dirs)
 
-        
     override def part1(input: Vector[String]) =
         val (root, dirs) = parse(input)
         dirs.filter(_.size <= 100000).map(_.size).sum.toString
 
     override def part2(input: Vector[String]) =
         val (root, dirs) = parse(input)
-        val usedSize = root.size.toLong
-        val maxSize = 70000000L
-        val freeSize = maxSize - usedSize
-        val neededSize = 30000000L
-        dirs.filter(_.size >= neededSize - freeSize).map(_.size).min.toString
+        dirs.filter(_.size >= 30000000L - (70000000L - root.size.toLong)).map(_.size).min.toString
